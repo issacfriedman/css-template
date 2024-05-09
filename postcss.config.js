36c29d2c48235241c28f4d5c28b99eb011266532
issacfriedman <issacfriedman613@gmail.com>
2024-05-08 20:10:52 -0400

    init

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+import postcssImport from "postcss-import";
+import postcssPresetEnv from "postcss-preset-env";
+
+export default {
+  plugins: [postcssImport(), postcssPresetEnv({ stage: 0 })],
+};
